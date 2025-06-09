abc964c013ca2227f23b51b643dbd88ebca34598
Daniel Mitka <danielmitka@Daniels-MacBook-Air.local>
2025-06-09 10:24:42 +0200

    Zkouska uprava obrazku at se zobrazi ve vercel deploymentu

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./styles/globals.css";
 //import "./styles/custom.css"; // <== přidáš tohle
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import { fontFamily, roboto, montserrat } from "./fonts";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -15,14 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto:wght@300;400;700;900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body style={{ fontFamily: "'Roboto', 'Montserrat', sans-serif" }}>
+    <html lang="en" className={`${roboto.variable} ${montserrat.variable}`}>
+      <body
+        style={{
+          fontFamily: "var(--font-roboto), var(--font-montserrat), sans-serif",
+        }}
+      >
         <Navbar />
         <main>{children}</main>
         <Footer />
