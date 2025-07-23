262b050c1afbf1a60b686a43ebde0360af4ac945
Daniel Mitka <danielmitka@192-168-88-208.local>
2025-07-23 20:09:35 +0200

    Pradni google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -194,6 +194,17 @@ export default function RootLayout({
           gtag("config", "G-SYWRQ64G6V");
           `}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W2CRC6GL');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
         <Script id="theme-mode" strategy="beforeInteractive">
           {`
             (function () {
@@ -225,6 +236,16 @@ export default function RootLayout({
           fontFamily: "var(--font-roboto), var(--font-montserrat), sans-serif",
         }}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W2CRC6GL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
